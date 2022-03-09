c2c02283810e108cacc5cc2a6ff5723d6c25f351
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2022-03-09 11:25:36 -1000

    feat: add internal Link support via PrismicProvider

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,17 +1,29 @@
 /**
- * Implement Gatsby's Browser APIs in this file.
+ * Implement Gatsby's Server Rendering APIs in this file.
  *
- * See: https://www.gatsbyjs.com/docs/browser-apis/
+ * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-ssr/
  */
 
 import * as React from "react";
+import { Link as GatsbyLink } from "gatsby";
+import { PrismicProvider } from "@prismicio/react";
 import { PrismicPreviewProvider } from "gatsby-plugin-prismic-previews";
 
 import { repositoryConfigs } from "./src/prismicPreviews";
 
-// Adds a shared React Context for Prismic preview sessions.
+/**
+ * An adapter to support Gatsby's `<Link>` component when using `<PrismicLink>`.
+ */
+const GatsbyLinkShim = React.forwardRef(({ href, ...props }, ref) => {
+  return <GatsbyLink to={href} ref={ref} {...props} />;
+});
+GatsbyLinkShim.displayName = "GatsbyLinkShim";
+
+// Adds a shared React Context for Prismic components and preview sessions.
 export const wrapRootElement = ({ element }) => (
-  <PrismicPreviewProvider repositoryConfigs={repositoryConfigs}>
-    {element}
-  </PrismicPreviewProvider>
+  <PrismicProvider internalLinkComponent={GatsbyLinkShim}>
+    <PrismicPreviewProvider repositoryConfigs={repositoryConfigs}>
+      {element}
+    </PrismicPreviewProvider>
+  </PrismicProvider>
 );
